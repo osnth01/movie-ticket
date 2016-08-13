3ddb6798c0d3f9aef9f0ccdff4e0b9c06632e976
osnth01 <osnth01@gmail.com>
2016-08-13 15:02:04 -0400

    create initial state and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,14 @@
-import expect from 'expect'
-import movies from '../../src/reducers'
+const initialState = []
 
+for (let i = 1; i <= 8; i++) {
+  initialState.push({
+    id: i,
+    title: 'Movie ' + i.toString()
+  })
+}
+
+const movies = (state = initialState, action) => {
+  return state
+}
+
+export default movies
--- test/reducers/index.spec.js
+++ test/reducers/index.spec.js
@@ -1,3 +1,25 @@
 import expect from 'expect'
 import movies from '../../src/reducers'
 
+function setup() {
+  const initialState = []
+
+  for (let i = 1; i <= 8; i++) {
+    initialState.push({
+      id: i,
+      title: 'Movie ' + i.toString()
+    })
+  }
+
+  return {
+    initialState
+  }
+}
+
+describe('movies reducer', () => {
+  it('should have initial state', () => {
+    const { initialState } = setup()
+
+    expect(movies(undefined, {})).toEqual(initialState)
+  })
+})
