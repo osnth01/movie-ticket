409fe26ad34ae48b155a131978f6bc1ee33d6433
osnth01 <osnth01@gmail.com>
2016-08-14 18:32:37 -0400

    add sorting and filtering to movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,24 +2,44 @@ import React, { PropTypes } from 'react'
 import Movie from './Movie'
 import CheckBox from './CheckBox'
 
-const MovieList = ({ movies }) => (
-  <div>
-    <h1>Playing Today</h1>
+const MovieList = ({ movies, filters }) => {
 
-    <CheckBox />
-    <CheckBox />
-    <CheckBox />
+  const contains = (features, filters) => {
+    return filters.filter( f => !features.includes(f) ).length === 0
+  }
 
-    {
-      movies.map( (movie, i) => {
-        return ( <Movie key={i} movie={movie} /> )
-      } )
+  const movieFilter = (a, b) => {
+    if (contains(a.features, filters) && !contains(b.features, filters)) {
+      return -1
+    } else if ((contains(a.features, filters) === contains(b.features, filters)) && (a.id < b.id)) {
+      return -1
     }
-  </div>
-)
+
+    return 1
+  }
+
+  let sortedMovies = movies.slice(0).sort(movieFilter)
+
+  return (
+    <div>
+      <h1>Playing Today</h1>
+
+      <CheckBox />
+      <CheckBox />
+      <CheckBox />
+
+      {
+        sortedMovies.map( (movie, i) => {
+          return ( <Movie key={i} movie={movie} /> )
+        } )
+      }
+    </div>
+  )
+}
 
 MovieList.propTypes = {
-  movies: PropTypes.array.isRequired
+  movies: PropTypes.array.isRequired,
+  filters: PropTypes.array.isRequired
 }
 
 export default MovieList
--- test/components/MovieList.spec.js
+++ test/components/MovieList.spec.js
@@ -3,17 +3,44 @@ import expect from 'expect'
 import MovieList from '../../src/components/MovieList'
 import { mount } from 'enzyme'
 
-function setup() {
-  const props = []
-
-  for (let i = 1; i <= 8; i++) {
-    props.push({
-      id: i,
-      title: 'Movie ' + i.toString()
-    })
+function setup(initialState) {
+  const props = {
+    movies: [],
+    filters: []
   }
 
-  const component = mount(<MovieList movies={props}/>)
+  for (let i = 1; i <= 16; i++) {
+    let movie = {
+      id: i,
+      title: 'Movie ' + i.toString(),
+      features: [
+        'accessible'
+      ]
+    }
+
+    if (i >= 11 && i <= 12) {
+      movie.features.push('IMAX')
+    }
+
+    if (i >= 13 && i <= 14) {
+      movie.features.push('3D')
+    }
+
+    if (i > 14) {
+      movie.features.push('IMAX')
+      movie.features.push('3D')
+    }
+
+    props.movies.push(movie)
+  }
+
+  Object.assign(props, initialState)
+
+  const component = mount(
+    <MovieList
+      movies={props.movies}
+      filters={props.filters} />
+    )
 
   return {
     component,
@@ -34,12 +61,84 @@ describe('MovieList component', () => {
   it('should render the number of movies given', () => {
     const { component, props, movies } = setup()
 
-    expect(movies.length).toEqual(props.length)
+    expect(movies.length).toEqual(props.movies.length)
   })
 
   it('should render three CheckBox components', () => {
     const { checkboxes } = setup()
 
     expect(checkboxes.length).toEqual(3)
+  })
+
+  describe('movie filters', () => {
+    it('should sort the movies by id when no filter is given', () => {
+      const { movies, component, props } = setup()
+
+      expect(movies.at(0).text()).toBe(props.movies[0].title)
+      expect(movies.at(15).text()).toBe(props.movies[15].title)
+    })
+
+    it('should sort the movies by accessible', () => {
+      const { movies, props } = setup({
+        filters: ['accessible']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[0].title)
+      expect(movies.at(15).text()).toBe(props.movies[15].title)
+    })
+
+    it('should sort the movies by IMAX', () => {
+      const { movies, props } = setup({
+        filters: ['IMAX']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[10].title)
+      expect(movies.at(15).text()).toBe(props.movies[13].title)
+    })
+
+    it('should sort the movies by 3D', () => {
+      const { movies, props } = setup({
+        filters: ['3D']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[12].title)
+      expect(movies.at(15).text()).toBe(props.movies[11].title)
+    })
+
+    it('should sort the movies by accessible and IMAX', () => {
+      const { movies, props } = setup({
+        filters: ['accessible', 'IMAX']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[10].title)
+      expect(movies.at(15).text()).toBe(props.movies[13].title)
+    })
+
+    it('should sort the movies by accessible and 3D', () => {
+      const { movies, props } = setup({
+        filters: ['accessible', '3D']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[12].title)
+      expect(movies.at(15).text()).toBe(props.movies[11].title)
+    })
+
+    it('should sort the movies by IMAX and 3D', () => {
+      const { movies, props } = setup({
+        filters: ['IMAX', '3D']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[14].title)
+      expect(movies.at(15).text()).toBe(props.movies[13].title)
+    })
+
+    it('should sort the movies by accessible, IMAX, and 3D', () => {
+      const { movies, props } = setup({
+        filters: ['accessible', 'IMAX', '3D']
+      })
+
+      expect(movies.at(0).text()).toBe(props.movies[14].title)
+      expect(movies.at(15).text()).toBe(props.movies[13].title)
+    })
   })
 })
